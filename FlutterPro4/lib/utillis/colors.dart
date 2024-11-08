d2f72abf73490560d53aca4416fe1be9d6c8f8b8
100825 <100825@MACTECH.LOC>
2024-11-08 19:09:09 +0530

    initial commit

---
+++ FlutterPro4/lib/utillis/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final lightBlue = Color.fromRGBO(27, 211, 199, 1);
+
+final darkBlue = Color.fromRGBO(16, 127, 196, 1);
+final white_primary = Colors.white;
